3f7350eedab82ccaf6064fd47c4914ef231887e7
Pranav1999 <bestpranavishere@gmail.com>
2018-07-14 00:24:44 +0530

    Fixed some bugs

--- save.py
+++ save.py
@@ -22,25 +22,31 @@ def createNewPass(userID,  key):
 	saveFile(str(passList), key)
 
 def saveFile(text, key):
-	text = encrypt(key,text)
-	file = open('passwords.txt','w')
-	text = text.decode('utf-8')
-	file.write(text)
-	file.close()
+    text = encrypt(key,text)
+    file = open('passwords.txt','w')
+    text = text.decode('utf-8')
+    file.write(text)
+    file.close()
 
 def readFile(key):
-	file = open('passwords.txt', 'r')
-	text = file.read()
-	file.close()
-	if text == '':
-		saveFile(b'{}', key)
-	text = (decrypt(key, text)).decode('utf-8')
-	try:
-		passwords = ast.literal_eval(text)
-		return passwords
-	except SyntaxError:
-		print('Wrong key')
-		sys.exit()
+    try:
+        file = open('passwords.txt', 'r')
+    except FileNotFoundError:
+        file = open('passwords.txt', 'a')
+        file.close()
+        file = open('passwords.txt', 'r')
+    text = file.read()
+    if text == '':
+        saveFile(b'{}', key)
+        text = file.read()
+    file.close()
+    text = (decrypt(key, text)).decode('utf-8')
+    try:
+        passwords = ast.literal_eval(text)
+        return passwords
+    except SyntaxError:
+        print('Wrong key')
+        sys.exit()
 
 def save():
 	if os.geteuid() != 0:
